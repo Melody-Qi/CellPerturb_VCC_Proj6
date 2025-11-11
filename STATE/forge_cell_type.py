b53f20738dc86e32210fa877e1067699b7796f7d
qiyt <2943895132@qq.com>
2025-11-11 19:33:29 +0800

    train error

--- STATE/forge_cell_type.py
+++ STATE/forge_cell_type.py
@@ -1,5 +1,9 @@
 import anndata as ad
 
+import pandas as pd
+
 adata = ad.read_h5ad("../vcc_data/preprocessed_training_data_2000.h5ad")
-adata.obs["cell_type"] = "H1"   # 全部设为同一个类型
+# adata.obs["cell_type"] = "H1"   # 全部设为同一个类型
+
+adata.obs["cell_type"] = pd.Categorical(["H1"] * adata.n_obs)
 adata.write_h5ad("../vcc_data/preprocessed_training_data_2000withcelltype.h5ad")
